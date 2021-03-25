7a84165610391f750354af10aa267d46e6e4b54b
ibmjavanh01 <ibmjavanh01@iiht.tech>
2021-03-25 09:20:41 +0530

    first commit

---
+++ src/main/java/com/ibm/demo/OrderController.java
@@ -0,0 +1,35 @@
+package com.ibm.demo;
+
+import org.springframework.http.HttpRequest;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OrderController {
+	@PostMapping("/order")
+	String createOrder() {
+		return "success";
+	}
+
+	@GetMapping("/order")
+	String getOrder() {
+		return "order created";
+	}
+
+	@PutMapping("/order/{id}")
+	String updateOrder(@PathVariable("id") int orderId) {
+		System.out.println(orderId);
+		return "order updated";
+	}
+
+	@DeleteMapping("/order/{id}")
+	String deleteOrder(@PathVariable("id") int orderId, HttpRequest httpRequest) {
+		System.out.println(httpRequest.getHeaders());
+		System.out.println(orderId);
+		return "order deleted";
+	}
+}
