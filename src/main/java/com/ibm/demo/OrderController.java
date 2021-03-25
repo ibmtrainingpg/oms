4c9f81cbb87215eb2f773914a7d5b4adc6772cfe
ibmjavanh01 <ibmjavanh01@iiht.tech>
2021-03-25 14:23:02 +0530

    added status code 201 for create order

--- src/main/java/com/ibm/demo/OrderController.java
+++ src/main/java/com/ibm/demo/OrderController.java
@@ -4,6 +4,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,27 +12,29 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ibm.demo.entity.Order;
 import com.ibm.demo.service.OrderService;
 
-@RestController
+@RestController //Bean
 public class OrderController { //frontend
 	@Autowired //is used for DI
 	OrderService orderService;   // DI
 	@PostMapping("/order")
+	@ResponseStatus(code = HttpStatus.CREATED)
 	String createOrder(@RequestBody @Valid Order order, BindingResult bindingResult) {
 		if(bindingResult.hasErrors()) {
 			throw new IllegalArgumentException("Somethign went wrong. Plesae retry");
 		}
 		System.out.println(order);
-		return orderService.createOrder(order);
+		return orderService.createOrder(order); //delegate 
 	}
 
 	@GetMapping("/order")
 	String getOrder() {
-		return "order created";
+		return "get";
 	}
 
 	@PutMapping("/order/{id}")
